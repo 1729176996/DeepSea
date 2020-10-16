17f04d45177f9017af463e6401b7b540b362dfbb
1729176996 <1729176996qq.com>
2020-10-16 09:41:43 +0800

    2

--- js/index.js
+++ js/index.js
@@ -2,12 +2,62 @@ var vm = new Vue({
 	el: "#main",
 	data() {
 	  return {
-		msg:''
+		url:'',
+		apikey:'',
+		list:[]
 	  };
 	},
 	methods: {
 		init(){
-			
+			this.url = 'http://t.yushu.im';
+			this.apikey = '0df993c66c0c636e29ecbb5344252a4a';
+			this.getData('正在上映');
+		},
+		getData(type) {
+			var _this = this;
+			var params = {
+				apikey:_this.apikey
+			};
+			var url = '';
+			if(type=='正在上映'){
+				url = _this.url + "/v2/movie/in_theaters";
+			}else if(type=='即将上映'){
+				url = _this.url + "/v2/movie/coming_soon";
+			}else if(type=='Top250'){
+				url = _this.url + "/v2/movie/top250";
+			}else if(type=='口碑榜'){
+				url = _this.url + "/v2/movie/weekly";
+			}else if(type=='北美票房榜'){
+				url = _this.url + "/v2/movie/us_box";
+			}else if(type=='新片榜'){
+				url = _this.url + "/v2/movie/new_movies";
+			}else{
+				return;
+			}
+			try{
+				plus.nativeUI.showWaiting("正在加载...");
+			}catch(e){
+				console.log(e.message);
+			}
+			_this.list = [];
+			axios.get(url,{params}).then(function (data) {
+				try{
+					plus.nativeUI.closeWaiting();
+				}catch(e){
+					console.log(e.message);
+				}
+				if(data&&data.data){
+					if(data.data.subjects&&data.data.subjects.length>0){
+						_this.list = data.data.subjects;
+					}else{
+						_this.list = [];
+					}
+				}else{
+					mui.toast('查询失败');
+				}
+		
+		    
+			});
 		}
 	},
 	beforeCreate() {
@@ -15,20 +65,10 @@ var vm = new Vue({
 		  
 	  }, false);
 	},
-	beforeRouteLeave(to, from, next) {
-	    console.log('离开页面时执行')
-	    next();
-	},
-	beforeRouteEnter (to, from, next) {
-		console.log('进入页面时执行')
-	    next(vm => {
-	      vm.init();
-	    })
-	},
 	created() {
 		
 	},
 	mounted() {
-	  
+	  this.init();
 	}
 })
